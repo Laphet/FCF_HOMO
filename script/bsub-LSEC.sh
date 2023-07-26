d31e710b4662d33388a57cf9688fef99461db0f9
Changqing YE from LSEC <ye_changqing@outlook.com>
2023-07-26 18:43:19 +0800

    reports from LSEC

---
+++ script/bsub-LSEC.sh
@@ -0,0 +1,15 @@
+#BSUB -J precondSolver
+#BSUB -n 36
+#BSUB -o ../reports/test-precond-solver-single-cpu-Xeon-6140-%J.log
+#BSUB -e ../tmp/%J.lsf.err
+#BSUB -W 10
+#BSUB -q batch
+#BSUB -R "span[ptile=36]"
+
+cd ${LS_SUBCWD}/..
+source ../set-oneapi.sh
+# set OMP_NUM_THREADS _and_ export! 
+OMP_NUM_THREADS=$LSB_DJOB_NUMPROC 
+export OMP_NUM_THREADS
+
+./bin/main -single
