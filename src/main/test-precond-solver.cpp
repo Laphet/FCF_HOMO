18747d6cf3d644d86b4d697b9922109859800d28
cqye <ye_changqing@outlook.com>
2023-11-13 14:50:49 +0800

    Use cudaDeviceSynchronize()

--- src/main/test-precond-solver.cpp
+++ src/main/test-precond-solver.cpp
@@ -83,6 +83,8 @@ void gpuTestCase(int M, int N, int P)
   std::cout << "  common=" << timeMillSec << "ms" << std::endl;
 
   start = std::chrono::steady_clock::now();
+  // Need to synchronize?
+  CHECK_CUDA_ERROR(cudaDeviceSynchronize());
   cufctSolver<T> gpuSolver(M, N, P);
   gpuSolver.setTridSolverData(&dl[0], &d[0], &du[0]);
   T *rhs_d{nullptr};
@@ -90,21 +92,26 @@ void gpuTestCase(int M, int N, int P)
   CHECK_CUDA_ERROR(cudaMemcpy(reinterpret_cast<void *>(rhs_d), reinterpret_cast<void *>(&rhs[0]), size * sizeof(T), cudaMemcpyHostToDevice));
   end         = std::chrono::steady_clock::now();
   timeMillSec = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+  CHECK_CUDA_ERROR(cudaDeviceSynchronize());
   std::cout << "  Warm-up=" << timeMillSec << "ms" << std::endl;
 
   start = std::chrono::steady_clock::now();
+  CHECK_CUDA_ERROR(cudaDeviceSynchronize());
   gpuSolver.precondSolver(&rhs_d[0]);
-  end         = std::chrono::steady_clock::now();
+  end = std::chrono::steady_clock::now();
+  CHECK_CUDA_ERROR(cudaDeviceSynchronize());
   timeMillSec = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
   std::cout << "  precondSolver=" << timeMillSec << "us" << std::endl;
 
   start = std::chrono::steady_clock::now();
+  CHECK_CUDA_ERROR(cudaDeviceSynchronize());
   CHECK_CUDA_ERROR(cudaMemcpy(reinterpret_cast<void *>(&rhs[0]), reinterpret_cast<void *>(&rhs_d[0]), size * sizeof(T), cudaMemcpyDeviceToHost));
   CHECK_CUDA_ERROR(cudaFree(rhs_d));
   mkl::cblas::axpy(size, static_cast<T>(-1), &u[0], 1, &rhs[0], 1);
   T error     = mkl::cblas::nrm2(size, &rhs[0], 1) / std::sqrt(static_cast<T>(size));
   end         = std::chrono::steady_clock::now();
   timeMillSec = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+  CHECK_CUDA_ERROR(cudaDeviceSynchronize());
   std::cout << "  check=" << timeMillSec << "ms, error=" << error << std::endl;
 }
 #endif
